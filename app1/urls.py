70c8a3c9c63a75f1032a4785dffbe1ed7eb015d4
Niyaszubair <niyasniyu5757@gmail.com>
2022-09-03 23:59:10 +0530

    first commit

--- app1/urls.py
+++ app1/urls.py
@@ -5,6 +5,7 @@ from.import views
 urlpatterns = [
 
     path('login',views.login,name='login'),
+    path('logout',views.logout,name='logout'),
     path('register',views.register,name='register'),
     path('base',views.base,name='base'),
 
